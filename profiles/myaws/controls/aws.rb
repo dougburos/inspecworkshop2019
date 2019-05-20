fa49ab60bde13e8c0b44351a7ee100bc8dcbd6e2
Doug Buros <doug.w.buros@nordstrom.com>
2019-05-20 14:22:48 -0700

    add aws control

---
+++ profiles/myaws/controls/aws.rb
@@ -0,0 +1,13 @@
+control 'myaws-01' do
+  title 'AWS Verification'
+  desc 'Testing AWS'
+  describe aws_ec2_instance('i-03051ee9a499a0ddd') do
+    it { should exist }
+    it { should be_running }
+  end
+
+  describe aws_s3_bucket_object(bucket_name: 'inspec-jumpstart-2019', key: 'web01.html') do
+    it { should exist }
+    it { shnould be_public }
+  end
+end 
