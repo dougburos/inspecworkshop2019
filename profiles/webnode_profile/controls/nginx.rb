2f91f126c5fcd081a9979c34358a1942141559b3
Doug Buros <doug.w.buros@nordstrom.com>
2019-05-20 11:40:47 -0700

    clean up nginx.rb

--- profiles/webnode_profile/controls/nginx.rb
+++ profiles/webnode_profile/controls/nginx.rb
@@ -4,20 +4,12 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
+control 'mynginx-01' do
+  title 'Functional Tests'
+  desc 'Ensuring the web server is functioning correctly'
+  describe http('http://localhost', enable_remote_worker: true) do
+    its('status') { should cmp 200 }
+    its('body') { should cmp 'Inspec Jumpstart' }
+    its('headers.Content-Type') { should cmp 'text/html' }
   end
-end
-
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
-end
-describe http('http://localhost', enable_remote_worker: true) do 
-  its('status') { should cmp 200 }
-  its('body') { should cmp 'Inspec Jumpstart' }
-  its('headers.Content-Type') { should cmp 'text/html' }
 end
